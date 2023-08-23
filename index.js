3aac7bdcafc6fc10221b56f71e70808bafdce978
Arttu Kuitunen <arsaku@utu.fi>
2023-08-23 17:43:08 +0300

    changes to index.js -> server is now getting data from scrapeWeb function

--- index.js
+++ index.js
@@ -11,11 +11,8 @@ const app = express()
 app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)
 console.log("Page updated..")})
 
-//new data scraped from YOUTUBE
-//Only logs into console --> need an async await arrow function to be stored 
-
-const url = 'https://www.iltalehti.fi/'
-
+//new data scrape function for ILTALEHTI *** TOIMII *** 
+const url = 'https://www.iltalehti.fi'
 const scrapeWeb = async (page) => {
     try {
     const response = await axios.get(page)
@@ -24,15 +21,15 @@ const scrapeWeb = async (page) => {
     const news = []
 
     $('.full-article', html).each(function(){
-        const url = $(this).find('a').attr('href')
+        const page = (url)+$(this).find('a').attr('href')
         const title = $(this).find('.front-title').text()
         const time = $(this).find('.category-time').find('time').text()
         
-        news.push([
-            title,
-            url,
-            time
-        ])
+        news.push({
+            'title': title,
+            'url': page,
+            'time': time
+        })
     })
     return news
     }catch (err){
@@ -40,39 +37,16 @@ const scrapeWeb = async (page) => {
     }     
 }
 
-scrapeWeb(url).then(news => console.log(news))
-console.log('Website data should be here: ')
 
-
-//videodata example
-let videodata = [
-    {
-      id: 1,
-      content: "HTML is easy and fun",
-      date: "2022-01-10T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2022-01-10T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2022-01-10T19:20:14.298Z",
-      important: true
-    }
-  ]
 //Root heading
 app.get('/', (req, res) => {
     res.send('<h1> This is the root of the web application backend</h1>')
 })
-//setting up data api content
-app.get('/api/videodata', (req, res) => {
-    res.json(videodata)
-    console.log(videodata)
+//setting up data to api/newsdata ** TOIMII NYT **
+app.get('/api/newsdata', async (req, res) => {
+    const data = await scrapeWeb(url)
+    console.log('app.get api/newsdata käsiteltiin')
+    res.json(data)
 })
 //example of another data api content
 app.get('/api/else', (req, res) => {
